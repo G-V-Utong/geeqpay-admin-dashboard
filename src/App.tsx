cffa65f1918545390226f973e38053df5f6539ab
Godswill Utong <godswillutong@gmail.com>
2024-01-23 23:46:28 +0100

    Created routes and navbar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import Home from "./home/Home"
+import Home from "./home/Home";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Users from "./users/Users";
+import Products from "./products/Products";
 
 function App() {
 
-  return (
-    <Home/>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home/>
+      ),
+    },
+    {
+      path: "users",
+      element: (
+        <Users/>
+    )
+    },
+    {
+      path: "products",
+      element: (
+        <Products/>
+    )
+    },
+  ]);
+
+  return <RouterProvider router={router} />
 }
 
 export default App
