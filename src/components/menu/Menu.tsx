38aa5a326c206898ad3ae427ce3aea47690489c5
Godswill Utong <godswillutong@gmail.com>
2024-01-24 20:54:22 +0100

    Sidebar styling

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,40 +1,56 @@
-import './menu.scss';
-import'../../styles/global.scss';
-import { useState } from 'react';
-import { SidebarData } from '../sidebarData/SidebarData';
-import { Link } from 'react-router-dom';
+import "./menu.scss";
+import "../../styles/global.scss";
+import { useState } from "react";
+import { SidebarData } from "../sidebarData/SidebarData";
+import { Link } from "react-router-dom";
 
-const Menu = (
+const Menu = () =>
   // props: {onClick: () => void}
-  ) => {
+  {
+    const [showNav, setShowNav] = useState(false);
+    const showSidebar = () => setShowNav(!showNav);
 
-  const [ showNav, setShowNav ] = useState(false);
-  const showSidebar = () => setShowNav(!showNav)
+    return (
+      <>
+        <div className="sidebar">
+          <div className="logo">
+            <img
+              src="/public/VectorDashboard-logo.svg"
+              alt="logo"
+              onClick={showSidebar}
+              width={40}
+            />
+          </div>
 
-  return (
-    <>
-      <div 
-    // onClick={props.onClick}
-    >
-      <img src="/public/VectorDashboard-logo.svg" alt="logo" onClick={showSidebar} />
-    </div>
+          <nav className={showNav ? "menuContainer active" : "menuContainer"}>
+            <ul>
+              {SidebarData.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    className={
+                      showNav ? `${item.cName} active` : `${item.cName}`
+                    }
+                  >
+                    <Link
+                      to={item.path}
+                      className={showNav ? `itemList active` : `itemList`}
+                    >
+                      <img src={`${item.icon}`} alt={item.cName} />
+                      <span
+                        className={showNav ? "menuTitle active" : "menuTitle"}
+                      >
+                        {item.title}
+                      </span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </>
+    );
+  };
 
-    <nav className={showNav ? 'menuContainer active' : 'menuContainer'}>
-      <ul>
-        {SidebarData.map((item, index) =>{
-          return (
-            <li key={index} className={showNav ? `${item.cName} active` : `${item.cName}`}>
-              <Link to={item.path} className={showNav ? `itemList active` : `itemList`}>
-                <img src={`${item.icon}`} alt={item.cName} />
-                <span className={showNav ? 'menuTitle active' : 'menuTitle'}>{item.title}</span>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-    </nav>
-    </>
-  )
-}
-
-export default Menu
+export default Menu;
