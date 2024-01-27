4ed2bd27062c88e9620b89e4fed3f88b4e40dbdd
Godswill Utong <godswillutong@gmail.com>
2024-01-27 13:55:06 +0100

    toggle switch fixed

--- src/components/bottomBox/bottomRightBox/BottomRightBox.tsx
+++ src/components/bottomBox/bottomRightBox/BottomRightBox.tsx
@@ -1,7 +1,188 @@
+import './bottomRightBox.scss'
+
 const BottomRightBox = () => {
   return (
-    <div>
-      
+    <div className="bottomRightBox">
+      <div className="bottomRightBox__header">
+        <h1>
+        Top Platform 
+        </h1>
+        <p>
+        See All
+        </p>
+      </div>
+      <div className="bottomRightBox__body">
+        <div className="bottomRightBox__body__item1">
+            <h1>
+            Book Bazaar
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $2,500,000
+                </p>
+                <p>
+                +15%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item2">
+            <h1>
+            Artisan Aisle
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $1,800,000
+                </p>
+                <p>
+                +10%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item3">
+            <h1>
+            Toy Troop
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $1,200,000
+                </p>
+                <p>
+                +8%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item4">
+            <h1>
+            XStore
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $2,500,000
+                </p>
+                <p>
+                +15%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item5">
+            <h1>
+            App Bink
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $3,500,000
+                </p>
+                <p>
+                +18%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item6">
+            <h1>
+            Cannon Fodder
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $500,000
+                </p>
+                <p>
+                +5%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item7">
+            <h1>
+            Cafe Latte
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $1,500,000
+                </p>
+                <p>
+                +4%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item8">
+            <h1>
+            Shopify
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $1,200,000
+                </p>
+                <p>
+                +25%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item9">
+            <h1>
+            Ebay
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $1,600,000
+                </p>
+                <p>
+                +23%
+                </p>
+             </div>
+
+        </div>
+        <div className="bottomRightBox__body__item1">
+            <h1>
+            Senator 
+            </h1>
+             <div className='hr'>
+             <hr className='leftHr'/> <hr className='rightHr'/>
+             </div>
+             <div className="item1_text">
+                <p>
+                $2,500,000
+                </p>
+                <p>
+                +15%
+                </p>
+             </div>
+
+        </div>
+      </div>
     </div>
   )
 }
